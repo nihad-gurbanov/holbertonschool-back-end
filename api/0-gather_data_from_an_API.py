638a84302f5371ce0a936d4ff2a033e5bf0e15cc
gurbanovnihad <7404@holbertonstudents.com>
2024-04-27 15:38:52 +0400

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,32 +5,32 @@
     completed tasks from the JSONPlaceholder API.
     Usage: python3 script_name.py user_id
 """
+if __name__ == "__main__":
+    import requests
+    from sys import argv
 
-import requests
-from sys import argv
+    if len(argv) < 2:
+        exit()
 
-if len(argv) < 2:
-    exit()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos")
+    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    data = todo.json()
+    user_data = user.json()
 
-todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-user = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
-data = todo.json()
-user_data = user.json()
+    completed_task = 0
+    completed_task_list = []
+    total_tasks = 0
 
-completed_task = 0
-completed_task_list = []
-total_tasks = 0
+    username = user_data["name"]
+    for todo in data:
+        if todo["userId"] == int(argv[1]):
+            total_tasks += 1
+        if todo["userId"] == int(argv[1]) and todo['completed']:
+            completed_task += 1
+            completed_task_list.append('\t' + todo['title'])
 
-username = user_data["name"]
-for todo in data:
-    if todo["userId"] == int(argv[1]):
-        total_tasks += 1
-    if todo["userId"] == int(argv[1]) and todo['completed']:
-        completed_task += 1
-        completed_task_list.append('\t' + todo['title'])
-
-print(
-    f"Employee {username} is done with tasks({completed_task}/{total_tasks}):"
-    )
-for i in completed_task_list:
-    print(i)
+    print(
+        f"Employee {username} is done with tasks({completed_task}/{total_tasks}):"
+        )
+    for i in completed_task_list:
+        print(i)
