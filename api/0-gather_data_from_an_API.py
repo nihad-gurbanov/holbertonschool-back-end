97680a071f888155740f6b0154fa43533527109f
gurbanovnihad <7404@holbertonstudents.com>
2024-04-27 15:42:20 +0400

    API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,24 +13,26 @@ if __name__ == "__main__":
         exit()
 
     todo = requests.get("https://jsonplaceholder.typicode.com/todos")
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+        )
     data = todo.json()
     user_data = user.json()
 
-    completed_task = 0
+    completed = 0
     completed_task_list = []
-    total_tasks = 0
+    total = 0
 
     username = user_data["name"]
     for todo in data:
         if todo["userId"] == int(argv[1]):
-            total_tasks += 1
+            total += 1
         if todo["userId"] == int(argv[1]) and todo['completed']:
-            completed_task += 1
+            completed += 1
             completed_task_list.append('\t' + todo['title'])
 
     print(
-        f"Employee {username} is done with tasks({completed_task}/{total_tasks}):"
+        f"Employee {username} is done with tasks({completed}/{total}):"
         )
     for i in completed_task_list:
         print(i)
